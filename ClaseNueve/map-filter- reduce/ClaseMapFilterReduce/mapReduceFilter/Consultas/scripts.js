cbb842d906c95f450ac85f710b3be6cda5b6172f
santiagolo90 <santiago.lo90@gmail.com>
2017-11-14 16:34:41 -0300

    map-filter-reduce Actualizado

--- ClaseNueve/map-filter- reduce/ClaseMapFilterReduce/mapReduceFilter/Consultas/scripts.js
+++ ClaseNueve/map-filter- reduce/ClaseMapFilterReduce/mapReduceFilter/Consultas/scripts.js
@@ -9,24 +9,27 @@ var soluciones = {};
 
 // Retornar un array con los nombres de los usuarios femeninos
 
-soluciones.usuariosFemeninos = function(usuarios){
-    return usuarios.filter(function(user){
+soluciones.usuariosFemeninos = function (usuarios) {
+    return usuarios.filter(function (user) {
         return user.genero === 'Female';
-    }).map(function(user){
+    }).map(function (user) {
         return user.nombre;
     });
 }
 
 
-//console.log(soluciones.usuariosFemeninos(data));
+console.log(soluciones.usuariosFemeninos(data));
 
 // Retornar un array de strings (el email de los usuarios de sexo masculino)
 
-soluciones.mailsVarones = function(usuarios){
-    return usuarios.filter(function(user){
+soluciones.mailsVarones = function (usuarios) {
+    return usuarios.filter(function (user) {
         return user.genero === 'Male';
-    }).map(function(user){
-        return user.email + " " + user.genero;
+    }).map(function (user) {
+        return {
+            "email": user.email,
+            "Genero": user.genero
+        };
     });
 }
 
@@ -34,43 +37,128 @@ console.log(soluciones.mailsVarones(data));
 
 // Retornar un array de objetos que solo contengan las claves nombre, email y edad, de todos los usuarios mayores que 'edad'
 
+/*
 soluciones.usuariosMayores = function(usuarios, edad){
-    
+    return usuarios
+    .filter(function(user){
+        return user.edad > edad;
+    })
+    .map(function(user){
+        
+        x ={}
+        x.nombre= user.nombre,
+        x.email= user.email,
+        x.edad= user.edad
+        return x;
+
+    });
+}
+*/
+
+soluciones.usuariosMayores = function (usuarios, edad) {
+    return usuarios.filter(function (user) {
+        return user.edad > 17;
+    })
+        .map(function (user) {
+            return {
+                "Nombre": user.nombre,
+                "email": user.email,
+                "Edad": user.edad
+            };
+        });
+
 }
 
-//console.log(soluciones.usuariosMayores(data, 40));
+console.log(soluciones.usuariosMayores(data, 40));
 
-  // Retornar un objeto que contenga solo el nombre y la edad del usuario mas grande.
+// Retornar un objeto que contenga solo el nombre y la edad del usuario mas grande.
 
-soluciones.usuarioMasGrande = function(usuarios){
-    
+soluciones.usuarioMasGrande = function (usuarios) {
+    return usuarios.reduce(function (actual, siguente) {
+            if (actual.edad > siguente.edad) {
+                return {
+                    nombre: actual.nombre,
+                    edad: actual.edad
+                }
+
+            }
+            else {
+                return {
+                    nombre: siguente.nombre,
+                    edad: siguente.edad
+                }
+            }
+
+        });
+
 }
 
-//console.log(soluciones.usuarioMasGrande(data));
+console.log(soluciones.usuarioMasGrande(data));
 
 // Retornar el promedio de edad de los usuarios (number)
 
-soluciones.promedio = function(usuarios){
-  
+soluciones.promedio = function (usuarios) {
+    var acumEdad = usuarios.reduce(function(actual,siguente){
+        return actual + siguente.edad;
 
-    return (acumEdad /cantidad).toFixed(2);
+    },0);
+
+    var cantidad = usuarios.reduce(function(actual,siguente){
+        return actual + 1
+
+    },0);
+
+
+    return (acumEdad / cantidad).toFixed(2);
 }
 
-//console.log("Promedio edad usuarios " + soluciones.promedio(data));
+console.log("Promedio edad usuarios " + soluciones.promedio(data));
 
 // Retornar el promedio de edad de los usuarios hombres (number)
 
-soluciones.promedioVarones = function(usuarios){
-   
-   
+soluciones.promedioVarones = function (usuarios) {
+    var acumEdad = usuarios.filter(function (user) {
+        return user.genero === 'Male';
+    }).reduce(function(actual,siguente){
+        return actual + siguente.edad;
+
+    },0);
+
+    var cantidadHombres = usuarios.reduce(function(actual,siguente){
+        if(siguente.genero === "Male"){
+            return actual + 1
+        }
+        else
+        return actual;
+
+    },0);
+
+    return (acumEdad / cantidadHombres).toFixed(2);
 }
 
-//console.log("Promedio edad Varones " + soluciones.promedioVarones(data));
+console.log("Promedio edad Varones " + soluciones.promedioVarones(data));
 
- // Retornar el promedio de edad de los usuarios mujeres (number)
+// Retornar el promedio de edad de los usuarios mujeres (number)
 
-soluciones.promedioMujeres = function(usuarios){
-   
+soluciones.promedioMujeres = function (usuarios) {
+    var acumEdad = usuarios.filter(function (user) {
+        return user.genero === 'Female';
+    }).reduce(function(actual,siguente){
+        return actual + siguente.edad;
+
+    },0)
+
+    var cantidadMujeres = usuarios.reduce(function(actual,siguente){
+        if(siguente.genero === "Female"){
+            return actual + 1
+        }
+        else
+        return actual;
+
+    },0);
+
+    return (acumEdad / cantidadMujeres).toFixed(2);
+
 }
 
-//console.log("Promedio edad Mujeres " + soluciones.promedioMujeres(data));
+console.log("Promedio edad Mujeres " + soluciones.promedioMujeres(data));
